aeccdc5d444430202003370e4fc6e142a220704b
Nikita Tikhonyuk <nikitatikhonyuk@yandex.com>
2023-01-11 21:57:06 +1000

    Add some basics logic and user interface

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
+enum Player {
+    case human, computer
+}
+
 struct ContentView: View {
+    
+    let columnsGrid: [GridItem] = [GridItem(.flexible()),
+                                   GridItem(.flexible()),
+                                   GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            LazyVGrid(columns: columnsGrid) {
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Circle()
+                            .foregroundColor(.orange)
+                            .padding(.bottom, 6.6)
+                        Image(systemName: moves[index]?.indicator ?? "")
+                            .resizable()
+                            .frame(width: 40, height: 42)
+                            .foregroundColor(.white)
+                    }
+                    .onTapGesture {
+                        if isCircleOccupied(in: moves, forIndex: index) { return }
+                        moves[index] = Move(player: isHumanTurn ? .human : .computer, boardIndex: index)
+                        isHumanTurn.toggle()
+                    }
+                }
+            }
+            Spacer()
         }
         .padding()
+    }
+    
+    func isCircleOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: {$0?.boardIndex == index})
+    }
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        player == .human ? "xmark" : "circle"
     }
 }
 
